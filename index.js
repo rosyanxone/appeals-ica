8c7547d386b816137d6cfc48c0489b34c70c303e
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2024-08-01 06:29:43 +0800

    modify pdf

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs/promises");
+const { degrees, PDFDocument, rgb, StandardFonts } = require("pdf-lib");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log(req.query);
-  res.status(201).json({
-    status: "success",
-    data: "Hello! server is running",
+app.use("/", express.static("public"));
+
+app.get("/generate", async (req, res) => {
+  // Load the input PDF from a local file
+  const inputFile = "./files/Semester-8.pdf";
+  const existingPdfBytes = await fs.readFile(inputFile);
+
+  // Load PDF document from the binary data and embed Helvetica font into it asynchronously
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+  // Get all pages of the PDF document
+  const pages = pdfDoc.getPages();
+
+  // Get first page of the PDF
+  const firstPage = pages[0];
+
+  // Get the height & width of the first page of the PDF
+  const { width, height } = firstPage.getSize();
+
+  // Add diagonal red text to the first page
+  firstPage.drawText("x", {
+    x: width / 2,
+    y: height / 2,
+    size: 50,
+    font: helveticaFont,
+    color: rgb(0.1, 0.1, 0.1),
+    // rotate: degrees(-45),
+  });
+
+  // Save changes to the PDF document as binary data
+  const modifiedPdfBytes = await pdfDoc.save();
+
+  // Save the modified PDF in binary form to a file
+  fs.writeFile("./files/modified-pdf.pdf", modifiedPdfBytes).then(() => {
+    res.download("./files/modified-pdf.pdf");
   });
 });
 
-// Listen
 const port = process.env.PORT || 3000;
-app.listen(port);
-console.log("Listening on port: " + port);
+app.listen(port, () => {
+  console.log("Listening on port: " + port);
+});
